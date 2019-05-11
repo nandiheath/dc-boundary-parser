54be2675c77ad87821467338c3a3dbece141d04f
Nandiheath <nandi0315@gmail.com>
2019-05-11 20:29:30 +0800

    fix the library isn't reload file if the filename is the same

--- run.js
+++ run.js
@@ -7,34 +7,35 @@ const mkdirp = require('mkdirp');
 const BOUNDARY_KEYS = ['東', '東南', '南', '西南', '西', '西北', '北', '東北'];
 const debug = false;
 
-const DISTRCIT_CODE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T']
-const DISTRCIT_CODE_2003 = ['cnw', 'wc', 'east', 'south', 'ytm', 'ssp', 'kc', 'wts', 'kt', 'tw', 'tm', 'yl', 'north', 'tp',
-'sk', 'st', 'kwaitsing', 'islands'];
-const YEARS = ['2003', '2007', '2011', '2015', '2019'];
-const DOWNLOAD_FILE_PATH = 'tmp.pdf';
+const DISTRICT_CODE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T']
+const DISTRICT_CODE_2003 = ['cnw', 'wc', 'east', 'south', 'ytm', 'ssp', 'kc', 'wts', 'kt', 'tw', 'tm', 'yl', 'north', 'tp',
+  'sk', 'st', 'kwaitsing', 'islands'];
+const YEARS = [2007, 2011, 2015, 2019];
 
 async function run() {
-
+  console.log('Start to download the pdfs');
   for (const year of YEARS) {
-    for (const code of DISTRCIT_CODE) {
+    for (const code of DISTRICT_CODE) {
       try {
         await downloadAndParseData(year, code);
         console.log(`Finsihed process ${year}_${code}`);
+
       } catch (error) {
         console.error(`Cannot process ${year}_${code}.`);
         console.error(error);
       }
-
     }
   }
 
 }
 
-run().then();
+run().then().catch(err => {
+  console.error(err);
+});
 
 function getPdfPath(year, code) {
   switch (year) {
-    case 2003: return `https://www.eac.hk/pdf/distco/ch/2003dc_boundary/v2_${DISTRCIT_CODE_2013[DISTRCIT_CODE.indexOf(code)]}_c.pdf`;
+    case 2003: return `https://www.eac.hk/pdf/distco/ch/2003dc_boundary/v2_${DISTRICT_CODE_2003[DISTRICT_CODE.indexOf(code)]}_c.pdf`;
     case 2007: return `https://www.eac.hk/pdf/distco/2007dc/ch/${code}_descriptions.pdf`;
     case 2011: return `https://www.eac.hk/pdf/distco/2011dc/ch/${code}_descriptions.pdf`;
     case 2015: return `https://www.eac.hk/pdf/distco/2015dc/final/ch/${code}_descriptions.pdf`;
@@ -47,26 +48,41 @@ function saveToFile(year, code, data) {
   fs.writeFileSync(`output/${year}/${code}.json`, decodeURIComponent(JSON.stringify(data, null, 2)));
 }
 
+function getLocalStoragePdfPath(year, code) {
+  mkdirp.sync(`raw/pdf`);
+  return `raw/pdf/${year}_${code}.pdf`;
+}
+
 async function downloadAndParseData(year, code) {
   return new Promise((resolve, reject) => {
+    const path = getPdfPath(year, code);
+    const filePath = getLocalStoragePdfPath(year, code);
     request
-      .get(`https://www.eac.hk/pdf/distco/2015dc/final/ch/${code}_descriptions.pdf`)
-      .pipe(fs.createWriteStream(DOWNLOAD_FILE_PATH))
+      .get(path)
+      .pipe(fs.createWriteStream(filePath, {
+        flags: 'w+',
+        encoding: 'utf8'
+      }))
+      .on('error', (err) => {
+        console.log(err);
+        reject(err);
+      })
       .on('close', () => {
         // pdf is downloaded
-
-        let pdfParser = new PDFParser();
+        const pdfParser = new PDFParser(this);
         pdfParser.on("pdfParser_dataError", errData => {
+          console.error(errData);
           reject(errData);
         });
         pdfParser.on("pdfParser_dataReady", pdfData => {
           const pages = pdfData.formImage.Pages;
           const formattedData = parsePages(pages);
+
           saveToFile(year, code, formattedData);
           resolve();
         });
 
-        pdfParser.loadPDF(DOWNLOAD_FILE_PATH);
+        pdfParser.loadPDF(filePath);
       })
   })
 }
@@ -106,18 +122,41 @@ function parsePages(pages) {
   return processRows(rows);
 }
 
+function parsePopulationAndPercentage(row) {
+  let expectedPopulation = 0, deviationPercentage = 0;
+  // Join all the text and use regex to dig the population and percentage
+  const text = row.filter((_, index) => index >= 2).join('');
+  if (text !== null) {
+    const matches = text.match(/[+-]{0,1}\d*[\,\.]?\d+%?/g)
+    if (matches && matches.length >= 1) {
+      expectedPopulation = matches[0];
+    }
+    if (matches && matches.length >= 2) {
+      deviationPercentage = matches[1];
+    }
+  }
+
+
+  return {
+    expectedPopulation, deviationPercentage
+  };
+}
+
 function processRows(rows) {
   const results = [];
   let districtData = null;
   for (const row of rows) {
     const [key] = row;
     if (key.match(/\w(\d){2}/)) {
+      const {
+        expectedPopulation, deviationPercentage
+      } = parsePopulationAndPercentage(row);
       // We found the key and progress
       districtData = {};
-      districtData.code = key;
+      districtData.code = key.trim();
       districtData.name = row[1];
-      districtData.expectedPopulation = row[2];
-      districtData.deviationPercentage = row[3] + (row.length > 4 ? row[4] : '');
+      districtData.expectedPopulation = expectedPopulation;
+      districtData.deviationPercentage = deviationPercentage;
       districtData.bondaries = [];
       districtData.mainArea = [];
       results.push(districtData);
